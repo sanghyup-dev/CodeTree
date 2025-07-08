2c5d4734ad6cd2df9b83cdc754d0c1b40338d4e7
sanghyup-dev <hyupsang.dev@gmail.com>
2025-07-08 15:38:56 +0900

    Title: 변수 값 복사하기 3, Time: 91ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250708/변수 값 복사하기 3/copying-variable-values-3.java
@@ -0,0 +1,15 @@
+public class Main {
+
+  public static void main(String[] args) {
+    int a, b, c;
+    a = 1;
+    b = 5;
+    c = 3;
+    c = a;
+    a += c;
+    b -= c;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+  }
+}
