93ccd85052eed9e57d99c7ab23be6b40d4ed74a8
sanghyup-dev <hyupsang.dev@gmail.com>
2025-07-10 23:19:51 +0900

    Title: A 부터 B 까지, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250710/A 부터 B 까지/a-to-b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    while (true) {
+      System.out.print(a + " ");
+      if (a % 2 == 0) {
+        a += 3;
+      } else {
+        a *= 2;
+      }
+      if (a > b) {
+        break;
+      }
+    }
+  }
+}
