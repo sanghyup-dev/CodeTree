a7130989c42b063fecb6405d2ae0ab1c09479efd
sanghyup-dev <hyupsang.dev@gmail.com>
2025-11-20 13:37:48 +0900

    Title: 계단 오르기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251120/계단 오르기/climbing-stairs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] memo = new int[n+1];
+
+        memo[2] = 1;
+        if(n>2) memo[3] = 1;
+
+        for(int i = 4;i<=n;i++){
+            memo[i] = (memo[2] + memo[3])%10007;
+        }
+
+        System.out.println(memo[n]);
+    }
+}
