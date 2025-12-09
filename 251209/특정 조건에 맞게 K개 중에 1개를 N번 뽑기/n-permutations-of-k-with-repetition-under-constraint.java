a6ceda1f0b6f1e80de7311c5687fff794a088cf4
sanghyup-dev <hyupsang.dev@gmail.com>
2025-12-09 23:11:16 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251209/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main {
+
+    public static int n;
+    public static int k;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        k = sc.nextInt();
+        n = sc.nextInt();
+        iteration(0,-1,-1);
+    }
+
+    public static List<Integer> nums = new ArrayList<>();
+
+    public static void iteration(int it, int prev, int prevprev){
+        if(it==n){
+            for(int i: nums){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 1; i<=2; i++){
+            if(i == prev && i == prevprev) continue;
+            nums.add(i);
+            iteration(it+1, i, prev);
+            nums.remove(nums.size()-1);
+        }
+    }
+}
