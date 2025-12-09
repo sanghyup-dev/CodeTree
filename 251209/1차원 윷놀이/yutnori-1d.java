626207c546a4b52787dc36bf063d9909bf889d4c
sanghyup-dev <hyupsang.dev@gmail.com>
2025-12-09 23:43:08 +0900

    Title: 1차원 윷놀이, Time: 127ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251209/1차원 윷놀이/yutnori-1d.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Main {
+
+    public static int n;
+    public static int m;
+    public static int k;
+    public static int[] nums;
+    public static int[] horses;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt()-1;
+        k = sc.nextInt();
+        nums = new int[n];
+        horses = new int[k];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        iter(0);
+        System.out.println(maxCnt);
+    }
+
+    public static int maxCnt = 0;
+
+    public static void iter(int it){
+        if(it==n){
+            int cnt = 0;
+            for(int i: horses){
+                if( i>=m) cnt++;
+            }
+            maxCnt = Math.max(maxCnt, cnt);
+        }
+        for(int i = 0; i<k;i++){
+            if(horses[i]<m){
+                horses[i]+=nums[it];
+                iter(it+1);
+                horses[i]-=nums[it];
+            }
+        }
+    }
+
+
+}
