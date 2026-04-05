62e2db63855982a7e8fe224984e6db5dbe7fa0d4
sanghyup-dev <hyupsang.dev@gmail.com>
2026-04-05 15:33:32 +0900

    Title: 사각형 채우기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260405/사각형 채우기 2/rectangle-fill-2.java
+++ 260405/사각형 채우기 2/rectangle-fill-2.java
@@ -2,13 +2,20 @@ import java.util.*;
 
 public class Main {
 
+    public static int[] dp = new int[1002];
+
     public static int findNofPossibilities(int n){
-        if (n==0 || n==1) return 1;
-        return (findNofPossibilities(n-1) + findNofPossibilities(n-2)*2);
+        if (dp[n]!=0) {
+            return dp[n];
+        }
+
+        return dp[n] = ((findNofPossibilities(n-1) + findNofPossibilities(n-2)*2))%10007;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        dp[0] = 1;
+        dp[1] = 1;
 
         System.out.println(findNofPossibilities(sc.nextInt()));
     }
