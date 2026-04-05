d41359221c6a96a012ba64155c635a0955e4d83c
sanghyup-dev <hyupsang.dev@gmail.com>
2026-04-05 17:53:24 +0900

    Title: 알바로 부자 되기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260405/알바로 부자 되기/being-rich-by-working-part-time.java
+++ 260405/알바로 부자 되기/being-rich-by-working-part-time.java
@@ -12,7 +12,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System. some);
+        Scanner sc = new Scanner(System.in);
         if (!sc.hasNextInt()) return;
         int n = sc.nextInt();
 
