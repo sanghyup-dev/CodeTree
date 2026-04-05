28e65773448f918887c26254cca6fb4f2d806483
sanghyup-dev <hyupsang.dev@gmail.com>
2026-04-05 17:52:03 +0900

    Title: 알바로 부자 되기, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

--- 260405/알바로 부자 되기/being-rich-by-working-part-time.java
+++ 260405/알바로 부자 되기/being-rich-by-working-part-time.java
@@ -1,12 +1,10 @@
 import java.util.*;
 
 public class Main {
-
-    private static class Alba{
-        int s;
-        int e;
+    private static class Alba {
+        int s, e;
         long p;
-        public Alba(int s, int e, long p){
+        public Alba(int s, int e, long p) {
             this.s = s;
             this.e = e;
             this.p = p;
@@ -14,29 +12,36 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System. some);
+        if (!sc.hasNextInt()) return;
         int n = sc.nextInt();
 
         ArrayList<Alba> albas = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             albas.add(new Alba(sc.nextInt(), sc.nextInt(), sc.nextLong()));
         }
 
-        long[] dp = new long[n];
+        // CRITICAL: Sort by end time
+        albas.sort(Comparator.comparingInt(a -> a.e));
 
+        long[] dp = new long[n];
         dp[0] = albas.get(0).p;
 
-        for(int i = 1; i < n; i++){
-            dp[i] = Math.max(dp[i-1], albas.get(i).p);
-
-            for(int j = 0; j < i; j++){
-                if(albas.get(j).e < albas.get(i).s){
-                    dp[i] = Math.max(dp[i], albas.get(i).p + dp[j]);
+        for (int i = 1; i < n; i++) {
+            // Option 1: Don't take current job
+            long currentProfit = dp[i - 1];
+            
+            // Option 2: Take current job
+            long takeJob = albas.get(i).p;
+            for (int j = i - 1; j >= 0; j--) {
+                if (albas.get(j).e < albas.get(i).s) {
+                    takeJob += dp[j];
+                    break; // Found the latest non-overlapping job
                 }
             }
+            dp[i] = Math.max(currentProfit, takeJob);
         }
 
-        System.out.println(dp[n-1]);
+        System.out.println(dp[n - 1]);
     }
 }
