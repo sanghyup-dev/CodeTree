fdd4b2468575bc8dc547a36046837e13fce5a7dc
sanghyup-dev <hyupsang.dev@gmail.com>
2026-04-05 17:37:52 +0900

    Title: 알바로 부자 되기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260405/알바로 부자 되기/being-rich-by-working-part-time.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Main {
+
+    private static class Alba{
+        int s;
+        int e;
+        int p;
+        public Alba(int s, int e, int p){
+            this.s = s;
+            this.e = e;
+            this.p = p;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Alba> albas = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            albas.add(new Alba(sc.nextInt(), sc.nextInt(), sc.nextInt()));
+        }
+    
+        int[] dp = new int[1003];
+
+        dp[0] = albas.get(0).p;
+        for(int i = 1; i<n; i++){
+            dp[i] = Math.max(dp[i-1], albas.get(i).p);
+            for(int j = 0; j<i; j++){
+                if(albas.get(j).e<albas.get(i).s) dp[i] = Math.max(dp[i], albas.get(i).p + dp[j]);
+            }
+        }
+
+        System.out.println(dp[n-1]);
+
+
+
+    }
+}
