dd1e0a2e63a6e782c971c40c33635bc96b58d0a3
sanghyup-dev <hyupsang.dev@gmail.com>
2026-04-05 17:49:41 +0900

    Title: 알바로 부자 되기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260405/알바로 부자 되기/being-rich-by-working-part-time.java
+++ 260405/알바로 부자 되기/being-rich-by-working-part-time.java
@@ -1,11 +1,12 @@
 import java.util.*;
+
 public class Main {
 
     private static class Alba{
         int s;
         int e;
-        int p;
-        public Alba(int s, int e, int p){
+        long p;
+        public Alba(int s, int e, long p){
             this.s = s;
             this.e = e;
             this.p = p;
@@ -15,24 +16,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
         ArrayList<Alba> albas = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-            albas.add(new Alba(sc.nextInt(), sc.nextInt(), sc.nextInt()));
+            albas.add(new Alba(sc.nextInt(), sc.nextInt(), sc.nextLong()));
         }
-    
-        int[] dp = new int[1003];
+
+        long[] dp = new long[n];
 
         dp[0] = albas.get(0).p;
-        for(int i = 1; i<n; i++){
+
+        for(int i = 1; i < n; i++){
             dp[i] = Math.max(dp[i-1], albas.get(i).p);
-            for(int j = 0; j<i; j++){
-                if(albas.get(j).e<albas.get(i).s) dp[i] = Math.max(dp[i], albas.get(i).p + dp[j]);
+
+            for(int j = 0; j < i; j++){
+                if(albas.get(j).e < albas.get(i).s){
+                    dp[i] = Math.max(dp[i], albas.get(i).p + dp[j]);
+                }
             }
         }
 
         System.out.println(dp[n-1]);
-
-
-
     }
 }
